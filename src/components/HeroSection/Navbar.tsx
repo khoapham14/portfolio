9c05e55db024e7f8a1e8bbc334fdf02c1761ecd0
khoapham14 <cody.pham14@gmail.com>
2023-03-09 15:21:26 +1300

    Adding mobile menu.

--- src/components/HeroSection/Navbar.tsx
+++ src/components/HeroSection/Navbar.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
-import Logo from '../../assets/logo_white.svg';
+import { useState, useEffect } from "react";
+import Logo from "../../assets/logo_white.svg";
+import Hamburger from "../../assets/hamburger_menu.svg";
 import "./Navbar.css";
 import "../Global.css";
 
 function Navbar() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   //Function to scroll to html element
   function scrollToElement(element: string) {
@@ -13,15 +15,54 @@ function Navbar() {
     }
   }
 
+  function openNav() {
+    document.getElementById("mobileNav")!.style.height = "100%";
+  }
+
+  function closeNav() {
+    document.getElementById("mobileNav")!.style.height = "0%";
+  }
+
+  //function to handle window resize
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      console.log(windowWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowWidth]);
+
   return (
     <div className="navbar">
-      <div className="logo-container"><img src={Logo} className="logo" /></div>
-      <div className="nav-links-container">
-        <div onClick={() => scrollToElement("projects")}>Projects</div>
-        <div onClick={() => scrollToElement("about")}>About</div>
-        <div onClick={() => scrollToElement("contact")}>Contact</div>
-        <div onClick={() => scrollToElement("credits")}>Credits</div>
+      <div className="logo-container">
+        <img src={Logo} className="logo" />
       </div>
+      {windowWidth > 1000 ? (
+        <div className="nav-links-container">
+          <div className="nav-link" onClick={() => scrollToElement("projects")}>Projects</div>
+          <div className="nav-link" onClick={() => scrollToElement("about")}>About</div>
+          <div className="nav-link" onClick={() => scrollToElement("contact")}>Contact</div>
+          <div className="nav-link" onClick={() => scrollToElement("credits")}>Credits</div>
+        </div>
+      ) : (
+        <>
+          <div className="hamburger" onClick={openNav}>
+            <img src={Hamburger} className="hamburger-icon" />
+          </div>
+          <div className="mobileNav" id="mobileNav">
+            <div className="close-btn" onClick={closeNav}>X</div>
+            <div className="nav-links-container">
+              <div className="nav-link" onClick={() => {scrollToElement("projects"); closeNav();}}>Projects</div>
+              <div className="nav-link" onClick={() => {scrollToElement("about"); closeNav();}}>About</div>
+              <div className="nav-link" onClick={() => {scrollToElement("contact"); closeNav();}}>Contact</div>
+              <div className="nav-link" onClick={() => {scrollToElement("credits"); closeNav();}}>Credits</div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
